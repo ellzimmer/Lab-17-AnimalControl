2f6acebcc7b6ce8b51d16a1dd7c99adc4a67f6f1
Ellen Zimmer <ellenzimmer711@gmail.com>
2015-11-19 16:38:27 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+var app = angular.module('myModule', ['ngRoute']);
+
+app.config(function($routeProvider){
+  	$routeProvider
+  		.when('/cat', 
+  			{
+    			templateUrl: 'cat.html',
+    			controller: 'animalCtrl'
+    		})
+ 		.when('/dog', 
+ 			{
+    			templateUrl: 'dog.html',
+    			controller: 'animalCtrl'
+  			})
+ 		.when('/hedgehog', 
+ 			{
+    			templateUrl: 'hedgehog.html',
+    			controller: 'animalCtrl'
+  			})
+ 		.when('/fish', 
+ 			{
+    			templateUrl: 'fish.html',
+    			controller: 'animalCtrl'
+  			})
+ 		$routeProvider.otherwise( 
+ 			{
+    			templateUrl: '404.html',
+    			controller: 'animalCtrl'
+  		});
+});
+
+app.controller('animalCtrl', function(){
+  
+});
